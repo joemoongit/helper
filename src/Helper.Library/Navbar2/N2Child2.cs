3d82890b23ca59b0a89b0c545c80abfc109ce2a5
joemoon <joemoon817@gmail.com>
2018-11-30 00:46:52 -0800

    Third Commit

--- src/Helper.Library/Navbar2/N2Child2.cs
+++ src/Helper.Library/Navbar2/N2Child2.cs
@@ -16,6 +16,33 @@ namespace Helper.Library
             Child2WebElement.Click();
         }
 
+        private Dictionary<string, string> PopulateDictionary(Dictionary<string, string> someDictionary)
+        {
+            foreach (var item in AllMultiSelectItems)
+            {
+                someDictionary[item.Text] = item.GetAttribute("data-offset-index");
+            }
+            return someDictionary;
+        }
+
+        public N2Child2 SelectItemInMultiSelectDropdown(params string[] items)
+        {
+            Dictionary<string, string> MultiSelectItemsDictionary = new Dictionary<string, string> { };
+            MultiSelectField.Click();
+            PopulateDictionary(MultiSelectItemsDictionary);
+            IJavaScriptExecutor jse = (IJavaScriptExecutor)Driver;
+
+            foreach (string item in items)
+            {
+                MultiSelectField.Click();
+                jse.ExecuteScript("arguments[0].click();", MultiSelectItem(MultiSelectItemsDictionary[item]));
+            }
+            return this;
+        }
+
         public IWebElement Child2WebElement => Driver.FindElement(By.LinkText("Child2"));
+
+        public IWebElement MultiSelectField => Driver.FindElement(By.CssSelector("#templateForm > section:nth-child(2)"));
+        public IWebElement MultiSelectItem(string itemIndex) => Driver.FindElement(By.CssSelector("#msMultiSelectItem_listbox > li:nth=child(" + (int.Parse(itemIndex) + 1).ToString() + ")"));
     }
 }
