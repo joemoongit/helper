8407ef19ca4c494134f59f2d947c9ec483f2803e
joemoon <joemoon817@gmail.com>
2021-05-13 15:32:46 -0700

    cleaned up

--- src/Helper.Scripts/Scripts/LoginScript.cs
+++ src/Helper.Scripts/Scripts/LoginScript.cs
@@ -8,35 +8,23 @@ namespace Helper.Scripts
     public class LoginScript
     {
         protected Github github;
-        //protected Facebook fb;
-        //protected Gmail gmail;
-        //protected Youtube youtube;
 
         [TestInitialize]
         public void SetupTest()
         {
             github = new Github();
-            //fb = new Facebook();
-            //gmail = new Gmail();
-            //youtube = new Youtube();
         }
 
         [TestMethod]
         public void TestSites()
         {
             github.Login();
-            //fb.Login();
-            //gmail.Login();
-            //youtube.Login();
         }
 
         [TestCleanup]
         public void TeardownTest()
         {
             github.Quit();
-            //fb.Quit();
-            //gmail.Quit();
-            //youtube.Quit();
         }
     }
 }
